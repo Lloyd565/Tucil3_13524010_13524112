4da1a94b0e8eb72bc61e36a773f98e7cef58fa39
alivovue <cubapetuking@gmail.com>
2026-05-10 01:56:22 +0700

    feat : initial form

--- src/include/solver/idastar/IDAStar.hpp
+++ src/include/solver/idastar/IDAStar.hpp
@@ -1,1 +1,9 @@
+#pragma once
 
+#include "solver/SolverInput.hpp"
+#include "solver/SolverResult.hpp"
+
+class IDAStar {
+    public:
+        static SolverResult solve(const SolverInput& solverInput);
+};
--- src/src/solver/idastar/IDAStar.cpp
+++ src/src/solver/idastar/IDAStar.cpp
@@ -1,1 +1,132 @@
+#include "solver/idastar/IDAStar.hpp"
 
+#include <chrono>
+#include <optional>
+#include <stdexcept>
+#include <string>
+#include <unordered_map>
+#include <vector>
+
+#include "solver/Comparators.hpp"
+#include "game/Movement.hpp"
+#include "game/Rules.hpp"
+#include "heuristic/astar/AStarHeuristic.hpp"
+
+using namespace std;
+
+enum Result {
+    FOUND, 
+    NOTFOUND
+};
+
+Direction getDirectionFromMove(char move) {
+    switch (move) {
+        case 'U':
+            return Direction::Up;
+        case 'D':
+            return Direction::Down;
+        case 'L':
+            return Direction::Left;
+        case 'R':
+            return Direction::Right;
+        default:
+            throw runtime_error("Error : invalid solution move");
+    }
+}
+
+vector<State> buildSolutionSteps(
+    const Board& board, 
+    const State& startState, 
+    const string& moves
+) {
+    vector<State> tempSteps;
+    State currentState = startState;
+
+    tempSteps.push_back(currentState);
+
+    for (char move : moves) {
+        optional<State> nextState = Movement::slide(
+            board,
+            currentState,
+            getDirectionFromMove(move)
+        );
+
+        if (!nextState.has_value()) throw runtime_error("Error : invalid solution path");
+
+        currentState = nextState.value();
+        tempSteps.push_back(currentState);
+
+    }
+
+    return tempSteps;
+}
+
+Result dfs(
+    const Board& board, 
+    const State& currentState, 
+    int* threshold, 
+    int* iterationCount,
+    vector<State>* exploredStates,
+    State* finalResult
+) {
+    int f = currentState.getTotalCost() + AStarHeuristic::compute(board, currentState, "H1");
+
+    if (f > *threshold) return Result::NOTFOUND;
+
+    if (Rules::isGoalState(board, currentState)) return Result::FOUND;
+
+    int minNextThreshold = numeric_limits<int>::infinity();
+
+    vector<State> possibleMoves = Movement::getPossibleMoves(board, currentState);
+
+    for (State move : possibleMoves) {
+        exploredStates->push_back(move);
+        if (Rules::isGoalState(board, move)) {
+            finalResult = &move;
+            return Result::FOUND;
+        }
+        Result result = dfs(board, currentState, threshold, iterationCount, exploredStates, finalResult);
+        if (result = Result::FOUND) return Result::FOUND;
+
+        minNextThreshold = min(minNextThreshold, *threshold);
+    }
+
+    return Result::NOTFOUND;
+
+}
+
+
+static SolverResult solve(const SolverInput& solverInput) {
+    const Board& board = solverInput.getBoard();
+    const State& initialState = solverInput.getInitialState();
+
+    int threshold = 10;
+
+    auto startTime = chrono::high_resolution_clock::now();
+
+    int iterationCount = 0;
+    vector<State> exploredStates;
+
+    while (true) {
+        State finalState;
+        int cacheThreshold = threshold;
+        Result result = dfs(board, initialState, &threshold, &iterationCount, &exploredStates, &finalState);
+        
+        if (result == Result::FOUND) return SolverResult(
+            true, 
+            finalState.getMoves(),
+            finalState.getTotalCost(),
+            iterationCount,
+            startTime - chrono::high_resolution_clock::now(),
+            buildSolutionSteps(board, finalState, finalState.getMoves()),
+            exploredStates
+        );
+
+        if (cacheThreshold == numeric_limits<int>::infinity()) return SolverResult::notFound(
+            iterationCount, 
+            startTime - chrono::high_resolution_clock::now(),
+            exploredStates
+        )
+    }
+
+}
